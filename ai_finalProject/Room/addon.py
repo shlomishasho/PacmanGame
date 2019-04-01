44c2fa8a3880888663f92fbd63af7529e6b19975
tal cohen <tal.cohen2@nice.com>
2019-04-01 17:05:17 +0300

    added addon class for weapon or ammo

---
+++ ai_finalProject/Room/addon.py
@@ -0,0 +1,33 @@
+class Addon():
+    def __init__(self, addon_type, location_cor=None):
+        self._type = addon_type
+        self._loc_coordinate = location_cor
+        self._width = 10
+        self._height = 10
+
+    @property
+    def location(self):
+        return self.location
+
+    @location.setter
+    def location(self, new_loc):
+        self._loc_coordinate = new_loc
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    @width.setter
+    def location(self, new_width):
+        self._width = new_width
+
+    @_height.setter
+    def location(self, new_height):
+        self._height = new_height
+
+    def get_dimensions(self):
+        return self._width, self._height
